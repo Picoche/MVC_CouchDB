b49ce9beb9a6e0cf05eb32ecaa0407c1ab65be8f
Picoche <hombert.fabien@gmail.com>
2024-01-18 18:38:16 +0100

    retrait parseInts

--- app/controller/controller.js
+++ app/controller/controller.js
@@ -1,88 +1,88 @@
-const models = require("../model/model")
-const Joi = require("joi")
+const models = require("../model/model");
+const Joi = require("joi");
 
 const boutiqueAddSchema = Joi.object({
-    commercantId: Joi.string().required(),
-    adresse: Joi.string().required(),
-    nbOfRating: Joi.number().required(),
-    nom: Joi.string().required(),
-    numTel: Joi.string().required(),
-    openDays: Joi.string().required(),
-    rating: Joi.number().required(),
-    produits: Joi.array().required(),
-    categorieId: Joi.string().required(),
-})
+  commercantId: Joi.string().required(),
+  adresse: Joi.string().required(),
+  nbOfRating: Joi.number().required(),
+  nom: Joi.string().required(),
+  numTel: Joi.string().required(),
+  openDays: Joi.string().required(),
+  rating: Joi.number().required(),
+  produits: Joi.array().required(),
+  categorieId: Joi.string().required(),
+});
 
 const produitAddSchema = Joi.object({
-    boutiqueId: Joi.string().required(),
-    nbOfRating: Joi.number().required(),
-    nom: Joi.string().required(),
-    prix: Joi.number().required(),
-    quantite: Joi.number().required(),
-    rating: Joi.number().required(),
-})
+  boutiqueId: Joi.string(),
+  nbOfRating: Joi.number().required(),
+  nom: Joi.string().required(),
+  prix: Joi.number().required(),
+  quantite: Joi.number().required(),
+  rating: Joi.number().required(),
+});
 
 const boutiqueEditSchema = Joi.object({
-    _id: Joi.string().required(),
-    _rev: Joi.string().required(),
-    commercantId: Joi.string(),
-    adresse: Joi.string(),
-    nbOfRating: Joi.number(),
-    nom: Joi.string(),
-    numTel: Joi.string(),
-    openDays: Joi.string(),
-    rating: Joi.number(),
-    produits: Joi.array(),
-    categorieId: Joi.string(),
-})
+  _id: Joi.string().required(),
+  _rev: Joi.string().required(),
+  commercantId: Joi.string(),
+  adresse: Joi.string(),
+  nbOfRating: Joi.number(),
+  nom: Joi.string(),
+  numTel: Joi.string(),
+  openDays: Joi.string(),
+  rating: Joi.number(),
+  produits: Joi.array(),
+  categorieId: Joi.string(),
+});
 
 const produitEditSchema = Joi.object({
-    _id: Joi.string().required(),
-    _rev: Joi.string().required(),
-    boutiqueId: Joi.string(),
-    nbOfRating: Joi.number(),
-    nom: Joi.string(),
-    prix: Joi.number(),
-    quantite: Joi.number(),
-    rating: Joi.number(),
-})
+  _id: Joi.string().required(),
+  _rev: Joi.string().required(),
+  boutiqueId: Joi.string(),
+  nbOfRating: Joi.number(),
+  nom: Joi.string(),
+  prix: Joi.number(),
+  quantite: Joi.number(),
+  rating: Joi.number(),
+});
 
 exports.getBoutiques = (req, res) => {
-    return models.getBoutiques(req, res)
-}
+  return models.getBoutiques(req, res);
+};
 
 exports.getBoutiquesByUserId = (req, res) => {
-    return models.getBoutiquesByUserId(req, res)
-}
+  return models.getBoutiquesByUserId(req, res);
+};
 
 exports.getProduits = (req, res) => {
-    return models.getProduits(req, res)
-}
+  return models.getProduits(req, res);
+};
 
 exports.getProduitById = (req, res) => {
-    return models.getProduitById(req, res)
-}
+  return models.getProduitById(req, res);
+};
 
 exports.addBoutique = (req, res) => {
-    return models.addBoutique(req, res, boutiqueAddSchema)
-}
+  return models.addBoutique(req, res, boutiqueAddSchema);
+};
 
 exports.addProduit = (req, res) => {
-    return models.addProduit(req, res, produitAddSchema)
-}
+  return models.addProduit(req, res, produitAddSchema);
+};
 
 exports.removeBoutique = (req, res) => {
-    return models.removeBoutique(req, res)
-}
+  return models.removeBoutique(req, res);
+};
 
-exports.removeProduit= (req, res) => {
-    return models.removeProduit(req, res)
-}
+exports.removeProduit = (req, res) => {
+  return models.removeProduit(req, res);
+};
 
 exports.editBoutique = (req, res) => {
-    return models.editBoutique(req, res, boutiqueEditSchema)
-}
+  return models.editBoutique(req, res, boutiqueEditSchema);
+};
 
 exports.editProduit = (req, res) => {
-    return models.editProduit(req, res, produitEditSchema)
-}
+  return models.editProduit(req, res, produitEditSchema);
+};
--- app/router/router.js
+++ app/router/router.js
@@ -1,46 +1,46 @@
-const express = require("express")
-const routerLivres = express.Router()
+const express = require("express");
+const routerLivres = express.Router();
 
-const controllers = require("../controller/controller")
+const controllers = require("../controller/controller");
 
 routerLivres.get("/boutiques", (req, res) => {
-    controllers.getBoutiques(req, res)
-})
+  controllers.getBoutiques(req, res);
+});
 
 routerLivres.get("/boutiques/:userId", (req, res) => {
-    controllers.getBoutiquesByUserId(req, res)
-})
+  controllers.getBoutiquesByUserId(req, res);
+});
 
 routerLivres.get("/:boutique/produits", (req, res) => {
-    controllers.getProduits(req, res)
-})
+  controllers.getProduits(req, res);
+});
 
-routerLivres.get("/:produit", (req, res) => {
-    controllers.getProduitById(req, res)
-})
+routerLivres.get("/:boutique/produits/:produit", (req, res) => {
+  controllers.getProduitById(req, res);
+});
 
 routerLivres.post("/boutique", (req, res) => {
-    controllers.addBoutique(req, res)
-})
+  controllers.addBoutique(req, res);
+});
 
-routerLivres.post("/produit", (req, res) => {
-    controllers.addProduit(req, res)
-})
+routerLivres.post("/:boutique/produits/addProduit", (req, res) => {
+  controllers.addProduit(req, res);
+});
 
 routerLivres.delete("/boutiques/:boutique", (req, res) => {
-    controllers.removeBoutique(req, res)
-})
+  controllers.removeBoutique(req, res);
+});
 
 routerLivres.delete("/produits/:produit", (req, res) => {
-    controllers.removeBoutique(req, res)
-})
+  controllers.removeBoutique(req, res);
+});
 
 routerLivres.put("/boutiques/:boutique", (req, res) => {
-    controllers.editBoutique(req, res)
-})
+  controllers.editBoutique(req, res);
+});
 
 routerLivres.put("/produits/:produit", (req, res) => {
-    controllers.editProduit(req, res)
-})
+  controllers.editProduit(req, res);
+});
 
-module.exports = { routerLivres }
+module.exports = { routerLivres };
